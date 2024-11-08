33462cb5ca5b5fa3e8cc0c7271769a4bcb55e1ba
YuqiZhang <cenchen227@gmail.com>
2024-11-08 14:45:02 +0800

    Update AlipayConfig comments and imports
    Updated the comment in AlipayConfig to correct a typo and removed unused import statements, streamlining the file.

--- src/main/java/com/config/AlipayConfig.java
+++ src/main/java/com/config/AlipayConfig.java
@@ -1,12 +1,9 @@
 package com.config;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
 /* *
  *类名：AlipayConfig
  *功能：基础配置类
- *详细：设置帐户有关信息及返回路径
+ *详细：设置账户有关信息及返回路径
  *修改日期：2017-04-05
  *说明：
  *以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
